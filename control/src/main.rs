caad789b01e4546aec01c63bc7d6d89fdbedc81d
Stefanloen <Stefan.loen@ziggo.nl>
2025-09-16 15:01:32 +0200

    Add uart and led task

--- control/src/main.rs
+++ control/src/main.rs
@@ -8,6 +8,7 @@
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_rp::gpio;
+use embassy_rp::uart;
 use embassy_time::Timer;
 use gpio::{Level, Output};
 use {defmt_rtt as _, panic_probe as _};
@@ -26,15 +27,35 @@ pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
 ];
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
+    // Init peripherals
     let p = embassy_rp::init(Default::default());
-    let mut led = Output::new(p.PIN_25, Level::Low);
 
-    for i in 1..10 {
-        info!("Blink {}", i);
+    //GPIOS
+    let led: Output<'_> = Output::new(p.PIN_25, Level::Low);
+
+    // UART1
+    let config = uart::Config::default();
+    let uart: uart::Uart<'_, uart::Blocking> = uart::Uart::new_blocking(p.UART1, p.PIN_4, p.PIN_5, config);
+
+    spawner.spawn(uart_heartbeat(uart)).unwrap();
+    spawner.spawn(led_blink(led)).unwrap();
+}
+
+#[embassy_executor::task]
+async fn uart_heartbeat(mut uart: uart::Uart<'static, uart::Blocking>) {
+    loop {
+        uart.blocking_write("Hello there \r\n".as_bytes()).unwrap();
+        Timer::after_millis(1000).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn led_blink(mut led: Output<'static>) {
+    loop {
         led.set_high();
-        Timer::after_millis(i * 100).await;
+        Timer::after_millis(500).await;
         led.set_low();
-        Timer::after_millis(i * 100).await;
+        Timer::after_millis(500).await;
     }
 }
