ae37674cc0e8a6dfa02ea58923f521948dfcb697
Jort van Driel <10116429+JortvD@users.noreply.github.com>
2025-09-23 11:43:24 +0200

    Implement config and parsing filtering

--- control/src/types.rs
+++ control/src/types.rs
@@ -5,3 +5,21 @@ pub const LINE_LEN: usize = 256;
 
 pub type Line = String<LINE_LEN>;
 pub type Burst = Vec<Line, MAX_LINES>;
+
+pub struct Config {
+    pub pre_min_elevation: u32,
+    pub pre_max_elevation: u32,
+    pub pre_min_azimuth: u32,
+    pub pre_max_azimuth: u32,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            pre_min_elevation: 0,
+            pre_max_elevation: 90,
+            pre_min_azimuth: 0,
+            pre_max_azimuth: 360,
+        }
+    }
+}
