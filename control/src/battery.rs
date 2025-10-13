e814fe77f11606d742238716ddd58bdb95307fca
Stefanloen <stefan.loen@ziggo.nl>
2025-10-13 17:25:45 +0200

    average adc

--- control/src/battery.rs
+++ control/src/battery.rs
@@ -33,11 +33,25 @@ impl Battery {
     }
 
     pub async fn get_battery_voltage(&mut self) -> Result<u32, adc::Error> {
-        let raw = self.adc.read(&mut self.pin_voltage).await?;
-        // Convert to mV (assuming 3.3V reference, 12-bit ADC)
-        let voltage_mv = raw as f32* 3300.0 / 4096.0;
+        const NUM_SAMPLES: usize = 100;
+        const SAMPLE_DELAY_MS: u64 = 10; // 100 samples per second (100 * 10 ms = 1 s)
+
+        let mut sum: u32 = 0;
+
+        for _ in 0..NUM_SAMPLES {
+            sum += self.adc.read(&mut self.pin_voltage).await? as u32;
+            embassy_time::Timer::after_millis(SAMPLE_DELAY_MS).await;
+        }
+
+        // Compute average ADC reading
+        let avg_raw = sum as f32 / NUM_SAMPLES as f32;
+
+        // Convert to mV (assuming 3.3 V reference, 12-bit ADC)
+        let voltage_mv = avg_raw * 3300.0 / 4096.0;
+
         // Compensate for 1:1 voltage divider
         Ok((voltage_mv * 2.0) as u32)
     }
+
 
 }
