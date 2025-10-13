704ff23c0415075fb356907a9bd27cf137548278
Jort van Driel <10116429+JortvD@users.noreply.github.com>
2025-10-13 15:13:07 +0200

    Add ubx support

--- control/src/gnss.rs
+++ control/src/gnss.rs
@@ -9,6 +9,37 @@ pub struct GNSSSensor {
     awake: bool,
 }
 
+pub struct UBXCommand {
+    pub cls: u8,
+    pub id: u8,
+    pub length: u16,
+    pub payload: [u8; 256],
+}
+
+fn checksum(command: &UBXCommand) -> u16 {
+    let mut ck_a: u8 = 0;
+    let mut ck_b: u8 = 0;
+
+    ck_a = ck_a.wrapping_add(command.cls);
+    ck_b = ck_b.wrapping_add(ck_a);
+
+    ck_a = ck_a.wrapping_add(command.id);
+    ck_b = ck_b.wrapping_add(ck_a);
+
+    ck_a = ck_a.wrapping_add((command.length & 0xFF) as u8);
+    ck_b = ck_b.wrapping_add(ck_a);
+
+    ck_a = ck_a.wrapping_add((command.length >> 8) as u8);
+    ck_b = ck_b.wrapping_add(ck_a);
+
+    for i in 0..command.length as usize {
+        ck_a = ck_a.wrapping_add(command.payload[i]);
+        ck_b = ck_b.wrapping_add(ck_a);
+    }
+
+    ((ck_b as u16) << 8) | (ck_a as u16)
+}
+
 impl GNSSSensor {
     pub fn new(uart: uart::Uart<'static, uart::Async>) -> Self {
         Self { uart, awake: false }
@@ -92,5 +123,53 @@ impl GNSSSensor {
 
         info!("[meas] GNSS woke up");
         self.awake = true;
+    }
+
+    pub async fn rxm_pmreq(
+        &self,
+        duration: u32,
+        backup: bool,
+        force: bool,
+        uartrx: bool,
+        extint0: bool,
+        extint1: bool,
+        spics: bool,
+    ) -> UBXCommand {
+        let mut payload = [0u8; 256];
+        payload[4..8].copy_from_slice(&duration.to_le_bytes());
+        payload[8] = (backup as u8) | ((force as u8) << 1);
+        payload[12] = (uartrx as u8)
+            | ((extint0 as u8) << 1)
+            | ((extint1 as u8) << 2)
+            | ((spics as u8) << 3);
+        UBXCommand {
+            cls: 0x02,
+            id: 0x41,
+            length: 16,
+            payload,
+        }
+    }
+
+    pub async fn send_ubx(&mut self, command: UBXCommand) {
+        let mut packet: [u8; 263] = [0; 263];
+        packet[0] = 0xB5; // Sync char 1
+        packet[1] = 0x62; // Sync char 2
+        packet[2] = command.cls;
+        packet[3] = command.id;
+        packet[4] = (command.length & 0xFF) as u8;
+        packet[5] = (command.length >> 8) as u8;
+        for i in 0..command.length as usize {
+            packet[6 + i] = command.payload[i];
+        }
+        let checksum = checksum(&command);
+        packet[6 + command.length as usize] = (checksum & 0xFF) as u8;
+        packet[7 + command.length as usize] = (checksum >> 8) as u8;
+        info!("[meas] Sending UBX command {:?}", &packet[..8 + command.length as usize]);
+        match self.uart.write(&packet[..8 + command.length as usize]).await {
+            Ok(_) => {}
+            Err(e) => {
+                info!("[meas] Error sending UBX command: {}", e);
+            }
+        }
     }
 }
