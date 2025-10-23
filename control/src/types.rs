b4461b977642bb8f579cb72e6d05410af2b0128b
Jort van Driel <10116429+JortvD@users.noreply.github.com>
2025-10-23 15:33:01 +0200

    better

--- control/src/types.rs
+++ control/src/types.rs
@@ -113,13 +113,13 @@ impl Default for Config {
         Self {
             pre_min_elevation: 5,
             pre_max_elevation: 30,
-            pre_min_azimuth: 120,
-            pre_max_azimuth: 270,
+            pre_min_azimuth: 130,
+            pre_max_azimuth: 280,
 
             post_min_elevation: 5,
             post_max_elevation: 30,
-            post_min_azimuth: 120,
-            post_max_azimuth: 270,
+            post_min_azimuth: 130,
+            post_max_azimuth: 280,
 
             min_relative_height: 0.5,
             max_relative_height: 10.0,
